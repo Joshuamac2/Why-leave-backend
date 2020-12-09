4285d76868d60eaa7bebb62bdd020f35a9e32630
jotkafomat <k-j@xl.wp.pl>
2020-12-09 16:32:50 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/api'do
+    resources :countries, only[:index, :create, :show]
+    resources :causes, only[:index, :create, :show]
+  end
 end
