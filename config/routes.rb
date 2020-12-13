89f5ab03300b83b812a1862bed6e15482b03f9b5
jotkafomat <k-j@xl.wp.pl>
2020-12-13 11:02:13 +0000

    Add ArticlesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
     resources :countries, only: [:index, :create, :show]
     resources :causes, only: [:index, :create, :show]
+    resources :articles, only: [:index, :create, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_144159) do
+ActiveRecord::Schema.define(version: 2020_12_13_103203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.text "headline"
+    t.text "trailText"
+    t.string "byline"
+    t.string "shortUrl"
+    t.string "thumbnail"
+    t.text "body"
+    t.text "bodyText"
+    t.bigint "country_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["country_id"], name: "index_articles_on_country_id"
+  end
 
   create_table "causes", force: :cascade do |t|
     t.text "description"
@@ -33,4 +47,5 @@ ActiveRecord::Schema.define(version: 2020_12_09_144159) do
     t.float "longitude"
   end
 
+  add_foreign_key "articles", "countries"
 end
