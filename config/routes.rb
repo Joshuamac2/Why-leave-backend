cba9bed723edf5adcba8277c51ac8d73cfb472a0
Beca-g <rebekahgalliano@gmail.com>
2020-12-14 21:54:05 +0000

    Fixing offenses with Rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-    resources :countries, only: [:index, :create, :show]
-    resources :causes, only: [:index, :create, :show]
+  resources :countries, only: [:index, :create, :show]
+  resources :causes, only: [:index, :create, :show]
 end
