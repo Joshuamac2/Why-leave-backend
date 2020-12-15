5af6537a0be3e6b633c3f70db838f253b633d0ec
jotkafomat <k-j@xl.wp.pl>
2020-12-15 21:29:27 +0000

    change size of received articles

--- app/models/guardian.rb
+++ app/models/guardian.rb
@@ -6,6 +6,6 @@ class Guardian
   base_uri "https://content.guardianapis.com/"
 
   def self.news_story(country)
-   get("/search?api-key=#{ENV['GUARDIAN_API_KEY']}&tag=world/#{country}&show-fields=headline,trailText,byline,shortUrl,thumbnail,body,bodyText&page-size=50")["response"]["results"]
+   get("/search?api-key=#{ENV['GUARDIAN_API_KEY']}&tag=world/#{country}&show-fields=headline,trailText,byline,shortUrl,thumbnail,body,bodyText&page-size=10")["response"]["results"]
  end
 end
